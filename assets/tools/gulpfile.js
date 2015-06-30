1ddcf01b0020fb6730738b02c314acf0c7cdf200
John Barry <JohnBar216@gmail.com>
2015-06-29 22:55:28 -0500

    set up gulp for sass compilations

---
+++ assets/tools/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  gulp.src('../css/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compact'}))
+    .pipe(gulp.dest('../css'));
+});
+ 
+
+
+gulp.task('default', function () {
+  gulp.watch('../css/sass/**/*.scss', ['sass']);
+});
