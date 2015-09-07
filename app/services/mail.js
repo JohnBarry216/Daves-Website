920e9314be592fa3c45fb539b27e61fe31e83e00
John Barry <JohnBar216@gmail.com>
2015-09-07 13:25:37 -0500

    more work on email factory

--- app/services/mail.js
+++ app/services/mail.js
@@ -1,6 +1,7 @@
 app.factory('MailFactory', function(mailingInfo){
 	
 	this.validateEmailAddress = function(email) {
+
 		var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     	return re.test(email);
 	};
@@ -13,20 +14,49 @@ app.factory('MailFactory', function(mailingInfo){
 			return "Invalid Type";
 		}
 
+		//validate required propertie
+		switch (mailingInfo) {
+			//confirm and validate email
+			case !mailingInfo.email || !validateEmailAddress( mailingInfo.email ):
+				return "Invalid Email";
 
-		//validate required properties
+			//confirm subject
+			case !mailingInfo.subject:
+				return "Invalid Subject";
 
+			//case confirm message Body
+			 case !mailingInfo.messageBody:
+			 	return "Invalid Message Body";
+		}
 
-		return valid;
-	}
-
-	this.sendMail = function(mailingInfo) {
-		
-		validateInfo(mailingInfo);
-		
+		//everything is good if it reaches this point
+		return true;
 	};
 
 
+	this.sendMail = function(mailingInfo) {
+
+		var defer = $q.defer();
+		var returnObject = { sent: "", errorMessage: ""	};
+
+		//validate
+		var valid = validateInfo(mailingInfo);
+		if( typeof valid === 'string'){
+
+			returnObject.sent = "false";
+			returnOject.errorMessage = valid;
+			defer.reject();
+		} else {
+
+			
+
+		}
+		
+		return defer.promise;
+	};
+
+
+	// make functions public
 	return {
 		send: sendMail
 	};
